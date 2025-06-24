f4373ba1405020aa05f536753265b5e19f3068cc
Anna Odój <anjaodoj@gmail.com>
2025-06-24 12:21:34 +0200

    powiedzmy

--- kolkokrzyzyk.py
+++ kolkokrzyzyk.py
@@ -2,6 +2,7 @@
 
 import turtle
 import random
+import time
 
 ninja = turtle.Turtle()
 turtle.bgcolor("#C0D9D9")
@@ -92,16 +93,22 @@ symbol = None
 ilosc = 0
 akcja = 0
 blok = 1
+srodki = [[-200,200], [0,200], [200,200], [-200,0], [0,0], [200,0], [-200,-200], [0,-200], [200,-200]]
 wolne = [[-200,200], [0,200], [200,200], [-200,0], [0,0], [200,0], [-200,-200], [0,-200], [200,-200]]
+uzytkownik = []
+zwyciestwa = [[1,4,7], [2,5,8], [3,6,9], [1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7]]
+flaga = 0
 
 def rysunek(a,b):
     global ilosc   
     if symbol == "o" and ilosc < 5:
         kolko(a, b, ninja)
         ilosc += 1
+        time.sleep(1)
     elif symbol == "x" and ilosc < 5:
         krzyzyk(a, b, ninja)
         ilosc += 1
+        time.sleep(1)
 
 def przeciwnik(el):
     if symbol == "x":
@@ -114,6 +121,9 @@ def klik(x, y):
     global wolne
     global akcja
     global blok
+    global uzytkownik
+    global flaga
+    global srodki
 
     if -550 <= x <= -450 and 200 <= y <= 300:
         symbol = "o"
@@ -122,58 +132,89 @@ def klik(x, y):
         symbol = "x"
         blok = 0
 
-    if blok == 0:
+    if blok == 0 and flaga == 0:
         # 1
         if -300 <= x <= -100 and 100 <= y <= 300 and wolne[0] != 0:
             rysunek(-200, 200)
             wolne[0] = 0
             akcja = 1
+            uzytkownik += [1]
         # 2
         elif -100 <= x <= 100 and 100 <= y <= 300 and wolne[1] != 0:
             rysunek(0,200)
             wolne[1] = 0
             akcja = 1
+            uzytkownik += [2]
         # 3
         elif 100 <= x <= 300 and 100 <= y <= 300 and wolne[2] != 0:
             rysunek(200,200)
             wolne[2] = 0
             akcja = 1
+            uzytkownik += [3]
         # 4
         elif -300 <= x <= -100 and -100 <= y <= 100 and wolne[3] != 0:
             rysunek(-200,0)
             wolne[3] = 0
             akcja = 1
+            uzytkownik += [4]
         # 5
         elif -100 <= x <= 100 and -100 <= y <= 100 and wolne[4] != 0:
             rysunek(0,0)
             wolne[4] = 0
             akcja = 1
+            uzytkownik += [5]
         # 6
         elif 100 <= x <= 300 and -100 <= y <= 100 and wolne[5] != 0:
             rysunek(200,0)
             wolne[5] = 0
             akcja = 1
+            uzytkownik += [6]
         # 7
         elif -300 <= x <= -100 and -300 <= y <= -100 and wolne[6] != 0:
             rysunek(-200,-200)
             wolne[6] = 0
             akcja = 1
+            uzytkownik += [7]
         # 8
         elif -100 <= x <= 100 and -300 <= y <= -100 and wolne[7] != 0:
             rysunek(0,-200)
             wolne[7] = 0
             akcja = 1
+            uzytkownik += [8]
         # 9
         elif 100 <= x <= 300 and -300 <= y <= -100 and wolne[8] != 0:
             rysunek(200,-200)
             wolne[8] = 0
             akcja = 1
+            uzytkownik += [9]
+
+        for i in range(len(zwyciestwa)):
+            kombinacja = zwyciestwa[i]
+            wynik = all(liczba in uzytkownik for liczba in kombinacja)
+
+            if wynik == True:
+                wygrana = kombinacja
+                numer = wygrana[0]
+                wspolrzedne = srodki[numer - 1]
+                ninja.penup()
+                ninja.goto(wspolrzedne)
+                ninja.pendown()
+                for j in range(1, len(wygrana)):
+                    numer = wygrana[j]
+                    wspolrzedne = srodki[numer - 1]
+                    ninja.goto(wspolrzedne)
+                ninja.penup()
+                ninja.goto(0,325)
+                ninja.pendown()
+                ninja.write("Wygrałeś", align="center", font=("Times New Roman", 20, "normal"))
+                flaga = 1
+                break
 
         # komputer
         dostepne = [pole for pole in wolne if pole != 0]
         losowe_pole = random.choice(dostepne)
 
-        if akcja == 1:
+        if akcja == 1 and flaga == 0:
             przeciwnik(losowe_pole)
             index = wolne.index(losowe_pole)
             wolne[index] = 0
